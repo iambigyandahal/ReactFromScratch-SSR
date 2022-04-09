27cf2f57ac73df6c18ac24f573e8ca2ffaad2040
bl4ck <bl4ck@granight.localhost>
2022-04-09 18:32:10 +0545

    react v18 updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
 import './index.scss'
 
-ReactDOM.hydrate(
+const container = document.getElementById('root')
+const root = ReactDOM.hydrateRoot(container,
     <React.StrictMode>
         <Router>
             <App />
         </Router>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode> 
 )
